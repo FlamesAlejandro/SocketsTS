23c91307a3daf51e3a674c5143b8b9eda0097604
FlamesNewPc <flameskazu@gmail.com>
2023-08-25 19:30:24 -0400

    añadida la funcionalidad de ver los id de los clientes conectados

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -9,10 +9,22 @@ export const connectToServer = () => {
 
 const addListeners = (socket: Socket) => {
     const serverStatusLabel = document.querySelector('#server-status')!
+    const clientsUl = document.querySelector('#clients-ul')!
+    // socket para ver el estado del cliente
     socket.on('connect', () =>{
         serverStatusLabel.innerHTML = 'connected'
     })
     socket.on('disconnect', () =>{
         serverStatusLabel.innerHTML = 'disconnected'
+    })
+    // para ver en el servidor los id's de los clientes conectados
+    socket.on('clients-updated', (clients: string[]) => {
+        let clientsHtml = '';
+        clients.forEach( clientId => {
+            clientsHtml += `
+                <li>${ clientId }</li>
+            `
+        });
+        clientsUl.innerHTML = clientsHtml;
     })
 }
